cd1c4e1bfa53110ab9970cba07b05f625cff8aa8
Yamini Hariharan <yahari@iu.edu>
2026-02-11 19:21:49 -0500

    function is working

--- tt_gutenberg/data.py
+++ tt_gutenberg/data.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 AUTHORS_URL = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv"
-LANGUAGES_URL = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv"
+LANGUAGES_URL = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_metadata.csv"
 
 def load_gutenberg():
     gutenberg_authors = pd.read_csv(AUTHORS_URL)
-    gutenberg_languages = pd.read_csv(LANGUAGES_URL)
+    gutenberg_meta = pd.read_csv(LANGUAGES_URL)
 
     df = pd.merge(
         gutenberg_authors,
-        gutenberg_languages,
+        gutenberg_meta,
         on="gutenberg_author_id",
         how="inner"
     )
